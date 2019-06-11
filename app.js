eb8bcab9005d44f10447bed66c38f72bdaeb1a02
Jonathan Moore <jmjonemoore@gmail.com>
2019-06-11 12:36:18 -0500

    Hanled defualt case

--- app.js
+++ app.js
@@ -6,29 +6,45 @@ const app = express();
 app.use(morgan('common'));
 
 const playlist = require('./playstore.js')
+let filterPlaylist = [...playlist];
 
 app.get('/apps', (req, res) => {
-  const {sort, genres} = req.query
+  const { sort, genres } = req.query
+  const sortOptions = ['rating', 'app']
   const genreOptions = ['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card']
-  genreOptionsLowerCase = genreOptions.forEach(e => {
-    e.toLowerCase
-  })
-  let filterPlaylist = [...playlist]
-  
-  if (sort.toLowerCase() !== 'rating' || sort.toLowerCase() !== 'app') {
+  const genreOptionsLowerCase = genreOptions.map(e => 
+    e.toLowerCase()
+  )
+//Default case
+if(!genres && !sort){
+  return res.json(filterPlaylist)
+}
+  else if (!sortOptions.includes(sort.toLowerCase())) {
     return res.status(400).send(`Sort param must be 'rating' or 'app'`)
   }
 
-  
-  
-  if (!genreOptionsLowerCase.includes(genres.toLowerCase())) {
+  else if (!genreOptionsLowerCase.includes(genres.toLowerCase())) {
     return res.status(400).send(`Genre must be one of the following: 'Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card' `)
   }
-  
 
-res
-  .json(filterPlaylist)
-  
+  else if (genres) {
+    filterPlaylist = filterPlaylist.filter(app => app
+      .Genres
+      .toLowerCase()
+      .includes(genres.toLowerCase()))
+  }
+
+  else if (sort) {
+    filterPlaylist
+      .sort((a, b) => {
+        return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+      })
+  }
+
+  res
+    .json(filterPlaylist);
+
+
 })
 
 
